2dbe9bb45edd44d035040718fe47f128a0f5605c
ShanySB <shanyb.sb@gmail.com>
2026-03-04 21:30:50 +0000

    Fixed spelling mistake

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -3,26 +3,26 @@ from taskmanager import db
 
 class Category(db.Model):
     # schema for the Category model
-    id = db.column(db.Integer, primary_key=True)
-    Category_name = db.Column(db.String(25), unique=True, nullable=False)
-    task = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
-        # __repr__to represent itself in the form of a string
-        return self
+        # __repr__ to represent itself in the form of a string
+        return self.category_name
 
 
 class Task(db.Model):
-    # schema for the Category model
-    id = db.Column(db.integer, primary_key=True)
+    # schema for the Task model
+    id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
-    task_description = db.Colum(db.Text, nullable=False)
+    task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
-    Category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
-        # __repr__to represent itself in the form of a string
-        return "#{0}- Task: | Urgent: {2}".format(
+        # __repr__ to represent itself in the form of a string
+        return "#{0} - Task: {1} | Urgent: {2}".format(
             self.id, self.task_name, self.is_urgent
         )
